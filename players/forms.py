e0e113925d9d9af432853a6f29d5e551c68e62a8
try3d <rsarans186@gmail.com>
2024-03-07 07:37:00 +0530

    Add requirements.txt, images, etc

--- players/forms.py
+++
@@ -1,8 +0,0 @@
-from .models import Player
-from django.forms import ModelForm
-
-
-class add(ModelForm):
-    class Meta:
-        model = Player
-        fields = "__all__"
--- players/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("getPlayer/<str:pk>/", views.player, name="get_player"),
-    path("", views.index),
-    path("addplayer", views.addplayer),
-    path("player_rtm", views.player_rtm),
-]
